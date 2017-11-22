018ba9da407c247d028122581c3804bdc2861156
Marais Bastien <marais.bas@gmail.com>
2017-11-22 15:09:52 +0100

    add sql script

---
+++ Codes/sql/init_DB.sql
@@ -0,0 +1,42 @@
+/* Creation of tables  */
+
+drop table REAL_USER;
+drop table TMP_USER  ;
+drop table REQUESTS ;
+drop table PROTECT_FILES ;
+
+
+
+create table REAL_USER (
+email varchar(50) primary key,
+name varchar(30) not null,
+surname varchar(30) not null,
+status varchar(30) not null,
+password varchar(50) not null,
+department varchar(30) not null);
+
+create table TMP_USER (
+email varchar(50) primary key,
+name varchar(30) not null,
+surname varchar(30) not null,
+status varchar(30) not null,
+password varchar(50) not null,
+department varchar(30) not null);
+
+create table REQUESTS (
+id_requests number(8) primary key identity(0,1),
+creation_date date not null,
+delivery_date date not null,
+user_email varchar(50) references REAL_USER,
+num_copy number(3) not null,
+num_page number(3) not null,
+finition varchar(30) not null,
+TODO);
+
+create table PROTECT_FILES (
+title varchar(100) not null,
+author varchar(100) not null,
+editor varchar(100) not null,
+num_protect_page number(3) not null,
+num_copy number(3) not null,
+requests number(8) references REQUESTS);
