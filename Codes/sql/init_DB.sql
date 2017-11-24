da6010db5285f54e0a030f9a25fe8d9fb501e30d
Marais Bastien <marais.bas@gmail.com>
2017-11-24 22:52:39 +0100

    Correctif

--- Codes/sql/init_DB.sql
+++ Codes/sql/init_DB.sql
@@ -6,7 +6,7 @@ drop table REQUESTS ;
 drop table PROTECT_FILES ;
 
 create table REAL_USER (
-email varchar(50) primary key, check (email LIKE "%@%"),
+email varchar(50) primary key,
 name varchar(30) not null,
 surname varchar(30) not null,
 status varchar(30) not null,
@@ -14,7 +14,7 @@ password varchar(50) not null,
 department varchar(30) not null);
 
 create table TMP_USER (
-email varchar(50) primary key, check (email LIKE "%@%.%"),
+email varchar(50) primary key,
 name varchar(30) not null,
 surname varchar(30) not null,
 status varchar(30) not null,
@@ -22,19 +22,20 @@ password varchar(50) not null,
 department varchar(30) not null,);
 
 create table REQUESTS (
-id_requests number(8) primary key identity(0,1),
+id_request number(8) primary key identity(0,1),
+user_email varchar(50) references REAL_USER,
+path_file varchar(50) not null,
 creation_date date not null,
 delivery_date date not null,
-user_email varchar(50) references REAL_USER,
 num_copy number(3) not null,
 num_page number(3) not null,
-finition varchar(30) not null,
-TODO);
+finition varchar(30) not null);
 
 create table PROTECT_FILES (
+id_file number(8) primary key identity(0,1),
+request number(8) references REQUESTS,
 title varchar(100) not null,
 author varchar(100) not null,
 editor varchar(100) not null,
 num_protect_page number(3) not null,
-num_copy number(3) not null,
-requests number(8) references REQUESTS);
+num_copy number(3) not null);
