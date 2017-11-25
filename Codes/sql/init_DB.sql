83999504ed012b44e5e48b53dfdcda98d0152518
Saliou BA <32985233+KatakuriD@users.noreply.github.com>
2017-11-25 14:26:42 +0100

    Update init_DB.sql
    Ajout d'une description concise de chaques tables.

--- Codes/sql/init_DB.sql
+++ Codes/sql/init_DB.sql
@@ -1,10 +1,12 @@
-/* Creation of tables  */
-
+ /* Creation of tables  */
+/*On commence par supprimer toutes les tables que l'on va créer afin de s'assurer de ne pas créer de doublons*/
 drop table REAL_USER;
 drop table TMP_USER  ;
 drop table REQUESTS ;
 drop table PROTECT_FILES ;
 
+/* REAL_USER : Cette table est une table définitive qui va contenir toutes les informations concernant un utilisateur. Ces informations 
+   sont ajoutées si et seulement si l'admin valide son inscription */
 create table REAL_USER (
 email varchar(50) primary key,
 name varchar(30) not null,
@@ -13,6 +15,8 @@ status varchar(30) not null,
 password varchar(50) not null,
 department varchar(30) not null);
 
+/* TMP_USER : Cette table est une table temporaire qui va contenir toutes les informations concernant un utilisateur. Ces informations 
+sont ajoutées lors de son inscription sur le site et avant la validation par l'admin */
 create table TMP_USER (
 email varchar(50) primary key,
 name varchar(30) not null,
@@ -21,6 +25,8 @@ status varchar(30) not null,
 password varchar(50) not null,
 department varchar(30) not null,);
 
+/* REQUESTS : Cette table est celle qui va contenir toutes les informations relatives à une demande d'impression faite par
+   un utilisateur*/
 create table REQUESTS (
 id_request number(8) primary key identity(0,1),
 user_email varchar(50) references REAL_USER,
@@ -31,6 +37,8 @@ num_copy number(3) not null,
 num_page number(3) not null,
 finition varchar(30) not null);
 
+/* PROTECT_FILES : Cette table est celle qui va contenir toutes les informations relatives à une demande d'impression faite par
+   un utilisateur et qui contient des pages d'un fichier protégé*/
 create table PROTECT_FILES (
 id_file number(8) primary key identity(0,1),
 request number(8) references REQUESTS,
