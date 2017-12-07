4ba9d584c29f51108545446935fabdf49f4ee236
Bastien Marais <marais.bas@gmail.com>
2017-12-07 09:51:56 +0100

    Update inscription.php

--- Codes/html-php/pages/inscription.php
+++ Codes/html-php/pages/inscription.php
@@ -15,6 +15,15 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="../../css/style.css">
 	<link rel="shortcut icon" href="../../images/favicon.ico" type="image/x-icon"/>
+	    
+	<link rel="apple-touch-icon" sizes="180x180" href="../../images/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="../../images/favicon-16x16.png">
+	<link rel="manifest" href="../../js/manifest.json">
+	<link rel="mask-icon" href="../../images/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="theme-color" content="#ffffff">
+
+	    
         <title> Rejoignez nous !</title>
     </head>
 
