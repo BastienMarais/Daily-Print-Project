25711fd01dd7d790bd37e18a883174bb5737bb8c
Bastien Marais <marais.bas@gmail.com>
2017-12-07 10:20:35 +0100

    Add recherches and change professeur to demandeur

--- Codes/html-php/pages/inscription.php
+++ Codes/html-php/pages/inscription.php
@@ -88,7 +88,7 @@
 									<option value="INFO">INFO</option>
 									<option value="MMI">MMI</option>
 									<option value="GEII">GEII</option>
-									<option value="Recherches">Autre</option>
+									<option value="Recherches">Recherches</option>
 									<option value="Autre">Autre</option>
 								</select>
                             </div>
