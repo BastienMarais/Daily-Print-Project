a6f6152a290f916391c7ade5fc151a6a6b7817a4
Bastien Marais <marais.bas@gmail.com>
2017-12-21 09:30:31 +0100

    Add files via upload

--- Codes/html-php/index.php
+++ Codes/html-php/index.php
@@ -44,15 +44,15 @@
                     <legend>Se connecter : </legend>
                     <form class="form-horizontal" action="pages/accueil.php">
                         <div class="form-group">
-                            <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-                            <div class="col-sm-10">
+                            <label for="inputEmail3" class="col-sm-4 control-label">Email :</label>
+                            <div class="col-sm-8">
                                 <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
                             </div>
                         </div>
                         <div class="form-group">
-                            <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-                            <div class="col-sm-10">
-                                <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                            <label for="inputPassword3" class="col-sm-4 control-label">Mot de passe :</label>
+                            <div class="col-sm-8">
+                                <input type="password" class="form-control" id="inputPassword3" placeholder="Mot de passe">
                             </div>
                         </div>
                         <div class="col-sm-offset-2 col-sm-10">
