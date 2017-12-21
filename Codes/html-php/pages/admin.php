ce9fe48f6efc7476dfc38c218b25f7f2c5a66dc7
Marais Bastien <marais.bas@gmail.com>
2017-12-21 08:46:21 +0100

    Ajout partie admin

---
+++ Codes/html-php/pages/admin.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+        <meta name="description" content="Daily'Print Admin Panel">
+        <meta name="keywords" content="HTML,CSS, PHP, JavaScript">
+        <meta name="author" content="Daily'Print TEAM">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<!-- Déclatration des styles css et scripts js -->
+		<script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="../../css/style.css">
+		<link rel="shortcut icon" href="../../images/favicon.ico" type="image/x-icon"/>
+		<title> Panel Admin </title>
+	</head>
+	<body class="background-generic">
+		<nav class="navbar navbar-inverse">
+			<div class="container-fluid">
+				<ul class="nav navbar-nav">
+					<li>
+						<a href="admin.php#?users=False"> <img src="../../images/logo-petit.png" class="img-responsive center-block"/> </a>
+					</li>
+					<li class="active">
+						<a href="admin.php"> Demandes d'inscription en attente </a>
+					</li>
+					<li>
+						<a href="admin_users.php"> Liste des utilisateurs </a>
+					</li>
+					<li>
+						<a href="admin_aide.php"> Aide admin </a>
+					</li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li class="dropdown">
+						<a href="" class="dropdown-toggle" data-toggle="dropdown">
+							Admin@DailyPrint.fr
+							<span class="caret"></span>
+						</a>
+						<ul class="dropdown-menu">
+							<li><a href="param.php">Mes paramètres</a></li>
+							<li><a href="../index.php">Se déconnecter</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<br/>
+		<br/>
+		<div class="container-fluid">
+			<content class="container">
+				<h1 class="center"> TODO: admin.php</h1>
+			</content>
+		</div>	
+	</body>
+</html>
