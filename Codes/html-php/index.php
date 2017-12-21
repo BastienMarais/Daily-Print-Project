ce73788cd9e44bf06115bf3b08efc8f4ffd8325c
Bastien Marais <marais.bas@gmail.com>
2017-12-21 11:35:00 +0100

    add required to password

--- Codes/html-php/index.php
+++ Codes/html-php/index.php
@@ -44,15 +44,15 @@
                     <legend>Se connecter : </legend>
                     <form class="form-horizontal" action="pages/accueil.php">
                         <div class="form-group">
-                            <label for="inputEmail3" class="col-sm-4 control-label">Email :</label>
+                            <label for="inputEmail" class="col-sm-4 control-label">Email :</label>
                             <div class="col-sm-8">
-                                <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                                <input type="email" class="form-control" id="inputEmail" placeholder="Email">
                             </div>
                         </div>
                         <div class="form-group">
                             <label for="inputPassword3" class="col-sm-4 control-label">Mot de passe :</label>
                             <div class="col-sm-8">
-                                <input type="password" class="form-control" id="inputPassword3" placeholder="Mot de passe">
+                                <input type="password" class="form-control" id="inputPassword3" placeholder="Mot de passe" required="required">
                             </div>
                         </div>
                         <div class="col-sm-offset-2 col-sm-10">
