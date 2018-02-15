a86babb3a67c45c605178ef358f28209e3290c62
Bastien Marais <marais.bas@gmail.com>
2018-02-15 01:15:01 +0100

    Update oubli.php

--- Codes/html-php/pages/oubli.php
+++ Codes/html-php/pages/oubli.php
@@ -41,7 +41,7 @@
                     <legend>Mot de passe oublié : </legend>
                     <form class="form-horizontal">
                         <div class="form-group">
-                            <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                            <label for="inputEmail3" class="col-sm-2 control-label">Email :</label>
                             <div class="col-sm-10">
                                 <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
                             </div>
