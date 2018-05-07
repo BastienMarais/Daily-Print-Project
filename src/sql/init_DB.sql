bdb2c7b29e9606bb6a90a5ca33c4d6143c52468e
Bastien Marais <marais.bas@gmail.com>
2018-05-07 14:12:02 +0200

    Julien - Update DB

--- src/sql/init_DB.sql
+++ src/sql/init_DB.sql
@@ -1,9 +1,9 @@
  /* Creation of tables  */
 /*On commence par supprimer toutes les tables que l'on va créer afin de s'assurer de ne pas créer de doublons*/
-drop table REAL_USER;
-drop table TMP_USER  ;
-drop table REQUESTS ;
-drop table PROTECT_FILES ;
+drop table IF EXISTS REAL_USER;
+drop table IF EXISTS TMP_USER  ;
+drop table IF EXISTS REQUESTS ;
+drop table IF EXISTS PROTECT_FILES ;
 
 /* REAL_USER : Cette table est une table définitive qui va contenir toutes les informations concernant un utilisateur. Ces informations 
    sont ajoutées si et seulement si l'admin valide son inscription */
@@ -30,22 +30,22 @@ notification boolean not null);
 /* REQUESTS : Cette table est celle qui va contenir toutes les informations relatives à une demande d'impression faite par
    un utilisateur*/
 create table REQUESTS (
-id_request number(8) primary key identity(0,1),
+id_request INT NOT NULL AUTO_INCREMENT primary key,
 user_email varchar(50) references REAL_USER,
 path_file varchar(50) not null,
 creation_date date not null,
 delivery_date date not null,
-num_copy number(3) not null,
-num_page number(3) not null,
+num_copy integer(3) not null,
+num_page integer(3) not null,
 finition varchar(30) not null);
 
 /* PROTECT_FILES : Cette table est celle qui va contenir toutes les informations relatives à une demande d'impression faite par
    un utilisateur et qui contient des pages d'un fichier protégé*/
 create table PROTECT_FILES (
-id_file number(8) primary key identity(0,1),
-request number(8) references REQUESTS,
+id_file integer(8) AUTO_INCREMENT primary key,
+request integer(8) references REQUESTS,
 title varchar(100) not null,
 author varchar(100) not null,
 editor varchar(100) not null,
-num_protect_page number(3) not null,
-num_copy number(3) not null);
+num_protect_page integer(3) not null,
+num_copy integer(3) not null);
