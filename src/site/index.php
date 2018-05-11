72be70f3198413a2c4de89e51a1cee1f0fbc9212
Bastien Marais <marais.bas@gmail.com>
2018-05-11 23:27:07 +0200

    Add index and inscription with bootstrap V4

--- src/site/index.php
+++ src/site/index.php
@@ -1,26 +1,106 @@
 <!doctype html>
-<html lang="en">
+<html lang="fr">
     <head>
     
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="Daily Print connexion">
+        <meta name="description" content="Daily Print">
         <meta name="keywords" content="HTML,CSS, PHP, JavaScript">
         <meta name="author" content="Daily Print TEAM">
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-
-        <link rel="stylesheet" href="css/style.css">         
-        <title>Connexion</title>
+		
+		<!-- My css -->
+        <link rel="stylesheet" href="css/style.css">       
+		
+        <title>Daily Print | Connexion</title>
         
     </head>
     <body>
     
-        
+        <content class="container" role="main">
+		
+            <div class="row">
+                <div class="col-xs-2 col-sm-2 col-md-4 col-lg-4"></div>
+                <div class="col-xs-8 col-sm-8 col-md-4 col-lg-4">
+                    <img src="img/logo.png" class="img-fluid" alt="Responsive image" />
+                </div>
+                <div class="col-xs-2 col-sm-2 col-md-4 col-lg-4"></div>
+            </div>
+			
+            <br/>
+			
+            <div class="row ">
+                <div class="col-xs-2 col-sm-2 col-md-4 col-lg-4"></div>
+				
+                <div class="col-xs-8 col-sm-8 col-md-4 col-lg-4 panel">
+				
+                    <legend class="color-blue">Se connecter : </legend>
+					
+                    <form id="connexionForm" method="POST" action="scripts/connexion.php">
+						<div class="form-group">
+							<label for="inputEmail">Adresse email : </label>
+							<input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Email" required>
+						</div>
+						<div class="form-group">
+							<label for="inputPassword">Mot de passe :</label>
+							<input type="password" class="form-control" id="inputPassword" placeholder="Mot de passe" required>
+						</div>
+						<div class="form-group">
+							<button type="button" class="btn btn-link" data-toggle="modal" data-target="#forgetPassword">Mot de passe oublié</a>
+						</div>
+						<div class="form-group form-check">
+							<input type="checkbox" class="form-check-input" id="checkSouvenir">
+							<label class="form-check-label" for="checkSouvenir">Se souvenir de moi</label>
+						</div>
+					</form>
+					<div class="form-row">
+						<div class="col">
+							<button type="submit" class="btn btn-primary" form="connexionForm">Connexion</button>
+						</div>
+						<div class="col">
+							<form action="pages/inscription.php">
+								<button type="submit" class="btn btn-secondary">Créer un compte</button>
+							</form>
+						</div>
+					</div>
+					
+					
+				</div>
+				
+				<div class="col-xs-2 col-sm-2 col-md-4 col-lg-4"></div>
+				
+            </div>
+			
+		</content>
 
-
+		<!-- Modal -->
+		<div class="modal fade" id="forgetPassword" tabindex="-1" role="dialog" aria-labelledby="forgetPassword" aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title" id="exampleModalCenterTitle">Récupération de mot de passe</h5>
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<form id="forgetForm" method="POST" action="scripts/forgetPassword.php">
+							<div class="form-group">
+								<label for="inputForget">Adresse email : </label>
+								<input type="email" class="form-control" id="inputForget" aria-describedby="emailHelp" placeholder="Email" required>
+							</div>
+						</form>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-secondary" data-dismiss="modal">Retour</button>
+						<button type="submit" class="btn btn-primary" form="forgetForm">Envoyer</button>
+					</div>
+				</div>
+			</div>
+		</div>
 
 
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
--- src/site/pages/param.php
+++ src/site/pages/param.php
@@ -1,19 +1,21 @@
 <!doctype html>
-<html lang="en">
+<html lang="fr">
     <head>
     
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="Daily Print connexion">
+        <meta name="description" content="Daily Print">
         <meta name="keywords" content="HTML,CSS, PHP, JavaScript">
         <meta name="author" content="Daily Print TEAM">
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-
-        <link rel="stylesheet" href="css/style.css">         
-        <title>Connexion</title>
+		
+		<!-- My css -->
+        <link rel="stylesheet" href="../css/style.css">       
+		
+        <title></title>
         
     </head>
     <body>
